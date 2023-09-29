3f64bc194ab7c6d9b9116806b655ef9f08172aaa
Daniel Szabo <daniel.szabo99@outlook.com>
2023-09-29 17:38:38 +0300

    Initial Commit

---
+++ TestApp Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestApp Watch App
+//
+//  Created by Dániel Szabó on 29/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestApp Watch App/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp Watch App
+//
+//  Created by Dániel Szabó on 29/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
